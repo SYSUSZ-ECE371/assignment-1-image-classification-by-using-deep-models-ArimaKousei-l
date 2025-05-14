f60bc66f9b9e642bb4f86f1b72a3985a1ea1596d
ArimaKousei-l <lq2271608583@gmail.com>
2025-05-15 00:09:11 +0800

    git lfs commit

---
+++ Ex1/resnet50_finetune_flower.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4efd65fc46053fcd3c3b03fd473b1fd1f7fb7aa5e0835436f4b468f91ef9cb3b
+size 2095
---
+++ Ex2/main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a017f3f253d6b83b4e40967618d94ca06c6fb9ce4c06c2d149d1444e24d819f7
+size 4525
